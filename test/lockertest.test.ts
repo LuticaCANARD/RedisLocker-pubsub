b5a5bdf4daf608d6873f3285de2302e5bcb971fb
Lutica_CANARD <presan100@gmail.com>
2024-02-25 15:23:58 +0900

    fix : package info, test script

--- test/lockertest.test.ts
+++ test/lockertest.test.ts
@@ -1,10 +1,9 @@
-import { RedisLocker } from '../src/index.js';
+import { RedisLocker } from '../src/singleLock.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 test('Locking Test',async ()=>{
     // 이 경우, 상호 Locking이 되는지, Locking시 값변경이 이루어지지 않았는지를 검증하라.
-
     const connectionString = process.env['RedisLink'];
     const redisClients = [
         {
@@ -17,28 +16,35 @@ test('Locking Test',async ()=>{
     ];
 
     const tester = redisClients.map( c => new RedisLocker(c,'target'));
+    console.time('aa1');
 
     await tester[0].locking();
-    console.log(tester[0].nowLocking);
-    tester[1].asyncGetLock(()=>{
-        console.log(tester[1].nowLocking , '>>>><<<<');
-        console.log('GET!');
-        tester[1].release().then();
-    });
-    tester[2].asyncGetLock(()=>{
-        console.log(tester[2].nowLocking , '>>>><<<<');
-        console.log('GET!2');
-        //tester[1].release().then();
-    });
-    await sleep(1000);
+    //console.log(tester[0].nowLocking);
+    // tester[1].asyncGetLock(()=>{
+    //     console.log(tester[1].nowLocking , '>>>><<<<');
+    //     console.log('GET!');
+    //     tester[1].release().then();
+    // });
+    // tester[2].asyncGetLock(()=>{
+    //     console.log(tester[2].nowLocking , '>>>><<<<');
+    //     console.log('GET!2');
+    //     //tester[1].release().then();
+    // });
     await tester[0].release();
-    console.log(tester[0].nowLocking);
+    //console.log(tester[0].nowLocking);
     // expect(tester.map(async u=>{
     //     const v = await u.locking();
     //     return 
     // })).toEqual([
     //     1,2,3
     // ]);
+    console.timeEnd('aa1');
+
+    console.time('aa2');
+    await tester[0].locking();
+    await tester[0].release();
+
+    console.timeEnd('aa2');
 
 });
 
